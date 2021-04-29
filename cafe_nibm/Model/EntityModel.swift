dc2501aeb0da7b75fe7d2990b22f06ec80b75ce5
Felician Kudhoos <isharkfelician@gmail.com>
2021-04-29 19:59:11 +0530

    first commit

---
+++ cafe_nibm/Model/EntityModel.swift
@@ -0,0 +1,8 @@
+//
+//  EntityModel.swift
+//  cafe_nibm
+//
+//  Created by Felician Ishark on 2021-04-14.
+//
+
+import Foundation
---
+++ cafe_nibm/Util/SessionManager.swift
@@ -0,0 +1,8 @@
+//
+//  SessionManager.swift
+//  cafe_nibm
+//
+//  Created by Felician Ishark on 2021-04-28.
+//
+
+import Foundation
